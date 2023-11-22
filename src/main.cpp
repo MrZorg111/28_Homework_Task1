e3fa85dbf6a5ee597519020210949ba04b6d5912
MrZorg111 <yodji333@mail.ru>
2023-11-22 23:08:41 +0300

    НОвый вариант!

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,14 @@
 #include <thread>
 #include <mutex>
 #include <chrono>
-#include <map>
+#include <vector>
 
-std::map<int, std::string> swi;
+std::vector<std::string> tablo;
 std::mutex border;
 
-void swimming(std::string name, float speed, std::map<int, std::string> &tablo) {
+void swimming(std::string name, float speed, std::vector<std::string> &tablo) {
 	float distance = 0;
 	int time = 0;
-	border.lock();
 	do {
 		time += 1;
 		distance += speed;
@@ -19,13 +18,8 @@ void swimming(std::string name, float speed, std::map<int, std::string> &tablo) 
 		std::cout << "\t--" << name << " swam " << distance << " meters!" << std::endl;
 		std::this_thread::sleep_for(std::chrono::seconds(1));
 	} while (distance < 100);
-	
-	if (tablo.count(time)) {
-		tablo.find(time)->second += " " + name;
-	}
-	else {
-		tablo.insert(std::pair<int, std::string>(time, name));
-	}
+	border.lock();
+	tablo.push_back((std::to_string(time) + " \t " + name));
 	border.unlock();
 }
 
@@ -40,12 +34,12 @@ int main() {
 	std::cout << "Enter the speed of each athlete, also separated by a space! " << std::endl;
 	std::cin >> speed1 >> speed2 >> speed3 >> speed4 >> speed5 >> speed6;
 
-	std::thread swimmer1(swimming, swim1, speed1, std::ref(swi));
-	std::thread swimmer2(swimming, swim2, speed2, std::ref(swi));
-	std::thread swimmer3(swimming, swim3, speed3, std::ref(swi));
-	std::thread swimmer4(swimming, swim4, speed4, std::ref(swi));
-	std::thread swimmer5(swimming, swim5, speed5, std::ref(swi));
-	std::thread swimmer6(swimming, swim6, speed6, std::ref(swi));
+	std::thread swimmer1(swimming, swim1, speed1, std::ref(tablo));
+	std::thread swimmer2(swimming, swim2, speed2, std::ref(tablo));
+	std::thread swimmer3(swimming, swim3, speed3, std::ref(tablo));
+	std::thread swimmer4(swimming, swim4, speed4, std::ref(tablo));
+	std::thread swimmer5(swimming, swim5, speed5, std::ref(tablo));
+	std::thread swimmer6(swimming, swim6, speed6, std::ref(tablo));
 	swimmer1.join();
 	swimmer2.join();
 	swimmer3.join();
@@ -56,9 +50,9 @@ int main() {
 	std::this_thread::sleep_for(std::chrono::seconds(1));
 	
 	std::cout << "\n\n";
-
-	for (std::map<int, std::string>::iterator it = swi.begin(); it != swi.end(); it++) {
-		std::cout << "\t--During the time " << it->first << " seconds, the swimmer(s) " << it->second << " swam(s) a hundred meters!" << std::endl;
+	std::cout << "Time \t Name" << std::endl;
+	for (int t = 0; t < tablo.size(); t++) {
+		std::cout << tablo[t] << std::endl;
 	}
 	
 	
